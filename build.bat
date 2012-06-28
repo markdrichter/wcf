6ccac693fb9cc2cfe07dbfb75765868f47b7fa8f
Mark D. Richter <mrichter@thoughtworks.com>
2012-06-28 06:28:19 -0700

    add a trailing quote

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 echo Building %1
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild ServiceApp.sln /t:Rebuild /nologo /p:Configuration=%1 /p:SkipInvalidConfigurations=true /p:DeployOnBuild=True /p:DeployTarget=Package /p:MSDeployPublishMethod=InProc /p:MsDeployServiceUrl=%2 /p:DeployIISAppPath=%3 /p:DeployToDatabase=false /p:CreatePackageOnPublish=True /p:OutPath="Package
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild ServiceApp.sln /t:Rebuild /nologo /p:Configuration=%1 /p:SkipInvalidConfigurations=true /p:DeployOnBuild=True /p:DeployTarget=Package /p:MSDeployPublishMethod=InProc /p:MsDeployServiceUrl=%2 /p:DeployIISAppPath=%3 /p:DeployToDatabase=false /p:CreatePackageOnPublish=True /p:OutPath="Package"
